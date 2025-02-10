91259208ce5db34811c4168a60a14090aca2e96b
amankeldi.kurban <dimitesting31@gmail.com>
2025-02-10 18:58:28 +0200

    setting up CRUD functionality to bootcamps

--- src/controllers/bootcamps.ts
+++ src/controllers/bootcamps.ts
@@ -1,38 +1,93 @@
 import { Request, Response, NextFunction } from 'express';
+import Bootcamp from '../models/Bootcamp';
 
 //@desk     Get all bootcamps
 //@route    GET /api/v1/bootcamps
 //@access   Public
-export const getBootcamps = (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({sucess: true, message: 'Show all bootcamps' })
+export const getBootcamps = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const bootcamps = await Bootcamp.find()
+        res.status(200).json({
+            sucess: true,
+            data: bootcamps
+        })        
+    } catch (error) {
+        res.status(400).json({ success: false})
+    }
 }
 
 //@desk     Get single bootcamps
 //@route    GET /api/v1/bootcamp/:id
 //@access   Public
-export const getBootcamp = (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({sucess: true, message: `Show bootcamp ${req.params.id}`})
+export const getBootcamp = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id)
+
+        if(!bootcamp)
+        {
+            res.status(400).json({success: false})
+        }
+
+        res.status(200).json({sucess: true, data: bootcamp})
+    } catch (error) {
+        res.status(400).json({success: false})
+    }
 }
 
 //@desk     Create new bootcamp
 //@route    POST /api/v1/bootcamp/
 //@access   Private
-export const createBootcamp = (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({sucess: true, message: 'Create new bootcamp'})
+export const createBootcamp = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const bootcamp = await Bootcamp.create(req.body)
+        res.status(201).json({
+            sucess: true,
+            data: bootcamp
+        })        
+    } catch (error) {
+        res.status(400).json({ success: false})
+    }
 }
 
 //@desk     Update new bootcamp
-//@route    POST /api/v1/bootcamp/:id
+//@route    PUT /api/v1/bootcamp/:id
 //@access   Private
-export const updateBootcamp = (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({sucess: true, message: `Update bootcamp ${req.params.id}`})
+export const updateBootcamp = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true, 
+            runValidators: true
+        })
+
+        if(!bootcamp)
+            {
+                res.status(400).json({success: false})
+            }
+
+        res.status(200).json({sucess: true, data: bootcamp})
+
+    } catch (error) {
+        res.status(400).json({ success: false})
+    }
 }
 
 
 //@desk     Delete new bootcamp
-//@route    POST /api/v1/bootcamp/:id
+//@route    DELETE /api/v1/bootcamp/:id
 //@access   Private
-export const deleteBootcamp = (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({sucess: true, message: `Delete bootcamp ${req.params.id}`})
+export const deleteBootcamp = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+
+        if(!bootcamp)
+            {
+                res.status(400).json({success: false})
+            }
+
+        res.status(200).json({sucess: true, data: "bootcamp has been deleted"})
+
+    } catch (error) {
+        res.status(400).json({ success: false})
+    }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,8 @@ const bootcamps = bootcampRouter
 
 const app = express();
 
+app.use(express.json())
+
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
