d3351fb6a37d134d38037b7e8160fde53b642bb6
amankeldi.kurban <dimitesting31@gmail.com>
2025-02-10 18:06:00 +0200

    connection to MongoDB with mongoose and setting up the BootcampScheme

---
+++ config/db.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.DB_CONNECTION || 'undefined')
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+}
+
+export default connectDB
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan';
 import bootcampRouter from './routes/bootcamps';
-
-const bootcamps = bootcampRouter
+import connectDB from '../config/db';
 
 dotenv.config({path: './config/config.env'});
+
+connectDB()
+
+const bootcamps = bootcampRouter
 
 const app = express();
 
@@ -17,6 +20,11 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000; 
 
-app.listen(PORT, 
+const server = app.listen(PORT, 
     () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+process.on('unhandledRejection', (err: Error, promise) => {
+    console.log(`Error ${err.message}`)
+    server.close(()=> process.exit(1))
+})
